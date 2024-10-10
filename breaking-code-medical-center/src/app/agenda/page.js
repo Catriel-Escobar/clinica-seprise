726b815dd459ef9e379545f6625af8a68df16928
MaEugeniaB <eugebava@gmail.com>
2024-10-10 18:13:21 -0300

    modal

--- breaking-code-medical-center/src/app/agenda/page.js
+++ breaking-code-medical-center/src/app/agenda/page.js
@@ -1,173 +1,82 @@
+'use client'
 import Encabezado from "@/Components/Encabezado"
+import { useState } from "react";
 import { IoPersonAddSharp } from "react-icons/io5";
+import Modal from "@/Components/Modal";
 
-import Image from "next/image"
 
 
 const Agenda = () => {
-  const closeButton = {name:"close-modal"}
-  const openButton ={name:"open-modal"}
-  const modalContainer = {name:"modal-component-container"}
-  const modal = {name:"modal-container"}
+    const [active, setActive] = useState(false)
+    const horas = [
+        { hora: "8:00 a 9:00", turno: "Eugenia Bava" },
+        { hora: "9:00 a 10:00" , turno: ""},
+        { hora: "10:00 a 11:00" , turno: "Gustavo Baranda"},
+        { hora: "11:00 a 12:00" , turno: ""},
+        { hora: "12:00 a 13:00" , turno: ""},
+        { hora: "13:00 a 14:00" , turno: ""},
+        { hora: "14:00 a 15:00" , turno: "Alejandro Abadi" },
+        { hora: "15:00 a 16:00" , turno: ""},
+        { hora: "1600 a 17:00" , turno: "Catriel Escobar"},
+        { hora: "17:00 a 18:00" , turno: "Marcelo Galimberti"}
+    ]
+
+    const handleClick = () => {
+        setActive(!active)
+        console.log(active)
+    }
+
+    return (
+        <div>
+            <Encabezado titulo={"Agenda"} consigna={"Ingrese Id del médico"} />
+
+            <section className="bg-white dark:bg-gray-900 antialiased">
+                <div className="px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24">
+                    <div className="max-w-3xl mx-auto text-center">
+                        <h2 className="text-4xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white">
+                            Agenda diaria
+                        </h2>
+
+                        <div className="mt-4">
+                            <a href="#" title=""
+                                className="inline-flex items-center text-lg font-medium text-primary-600 hover:underline dark:text-primary-500">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="flow-root max-w-3xl mx-auto mt-8 sm:mt-12 lg:mt-16">
+                        <div className="-my-4 divide-y divide-gray-200 dark:divide-gray-700">
+
+                            {
+                                horas.map((item, index) => (
+                                    <div key={index} className="flex justify-between flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
+                                        <p className="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
+                                            {item.hora}
+                                        </p>
+
+                                        <p className="text-lg font-semibold text-gray-900 dark:text-white">
+                                            {item.turno}
+                                        </p>
+                                        <button onClick={handleClick} type="submit" className="m-2 rounded bg-[#87b9a5] px-4 py-4 text-white">
+                                            <IoPersonAddSharp />
+
+                                        </button>
+
+                                    </div>
+                                ))
+                            }
+
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            < Modal active={active} handleClick={handleClick} />
 
 
 
-  return (
-    <div>
-        <Encabezado titulo={"Agenda"} consigna={"Ingrese Id del médico"}/>  
-      
-        <section class="bg-white dark:bg-gray-900 antialiased">
-        <div class="px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24">
-          <div class="max-w-3xl mx-auto text-center">
-            <h2 class="text-4xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white">
-              Agenda diaria
-            </h2>
-
-            <div class="mt-4">
-              <a href="#" title=""
-                class="inline-flex items-center text-lg font-medium text-primary-600 hover:underline dark:text-primary-500">
-              </a>
-            </div>
-          </div>
-
-          <div class="flow-root max-w-3xl mx-auto mt-8 sm:mt-12 lg:mt-16">
-            <div class="-my-4 divide-y divide-gray-200 dark:divide-gray-700">
-             
-              <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                  08:00 - 09:00
-                </p>
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                   <input class=" w-auto border-none bg-transparent px-6 py-4 text-gray-400 outline-none focus:outline-none " type="add" name="add" placeholder="..." />
-                   <button type="submit" class="m-2 rounded bg-[#87b9a5] px-4 py-4 text-white">
-                   <IoPersonAddSharp />
-                  
-                </button>
-                </h3>
-              </div>
-
-              <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                  09:00 - 10:00
-                </p>
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                   <input class=" w-auto border-none bg-transparent px-6 py-4 text-gray-400 outline-none focus:outline-none " type="add" name="add" placeholder="..." />
-                   <button type="submit" class="m-2 rounded bg-[#87b9a5] px-4 py-4 text-white">
-                   <IoPersonAddSharp />
-                  
-                </button>
-                </h3>
-              </div>
-
-              <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                  10:00 - 11:00
-                </p>
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                   <input class=" w-auto border-none bg-transparent px-6 py-4 text-gray-400 outline-none focus:outline-none " type="add" name="add" placeholder="..." />
-                   <button type="submit" class="m-2 rounded bg-[#87b9a5] px-4 py-4 text-white">
-                   <IoPersonAddSharp />
-                  
-                </button>
-                </h3>
-              </div>
-
-              <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                  11:00 - 12:00
-                </p>
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                   <input class=" w-auto border-none bg-transparent px-6 py-4 text-gray-400 outline-none focus:outline-none " type="add" name="add" placeholder="..." />
-                   <button type="submit" class="m-2 rounded bg-[#87b9a5] px-4 py-4 text-white">
-                   <IoPersonAddSharp />
-                  
-                </button>
-                </h3>
-              </div>
-
-              <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                  12:00 - 13:00
-                </p>
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                   <input class=" w-auto border-none bg-transparent px-6 py-4 text-gray-400 outline-none focus:outline-none " type="add" name="add" placeholder="..." />
-                   <button type="submit" class="m-2 rounded bg-[#87b9a5] px-4 py-4 text-white">
-                   <IoPersonAddSharp />
-                  
-                </button>
-                </h3>
-              </div>
-
-              <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                  13:00 - 14:00
-                </p>
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                   <input class=" w-auto border-none bg-transparent px-6 py-4 text-gray-400 outline-none focus:outline-none " type="add" name="add" placeholder="..." />
-                   <button type="submit" class="m-2 rounded bg-[#87b9a5] px-4 py-4 text-white">
-                   <IoPersonAddSharp />
-                  
-                </button>
-                </h3>
-              </div>
-
-              <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                  14:00 - 15:00
-                </p>
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                   <input class=" w-auto border-none bg-transparent px-6 py-4 text-gray-400 outline-none focus:outline-none " type="add" name="add" placeholder="..." />
-                   <button type="submit" class="m-2 rounded bg-[#87b9a5] px-4 py-4 text-white">
-                   <IoPersonAddSharp />
-                  
-                </button>
-                </h3>
-              </div>
-
-              <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                  15:00 - 16:00
-                </p>
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                   <input class=" w-auto border-none bg-transparent px-6 py-4 text-gray-400 outline-none focus:outline-none " type="add" name="add" placeholder="..." />
-                   <button type="submit" class="m-2 rounded bg-[#87b9a5] px-4 py-4 text-white">
-                   <IoPersonAddSharp />
-                  
-                </button>
-                </h3>
-              </div>
-
-              <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                  16:00 - 17:00
-                </p>
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                   <input class=" w-auto border-none bg-transparent px-6 py-4 text-gray-400 outline-none focus:outline-none " type="add" name="add" placeholder="..." />
-                   <button type="submit" class="m-2 rounded bg-[#87b9a5] px-4 py-4 text-white">
-                   <IoPersonAddSharp />
-                  
-                </button>
-                </h3>
-              </div>
-
-              <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                  17:00 - 18:00
-                </p>
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                   <input class=" w-auto border-none bg-transparent px-6 py-4 text-gray-400 outline-none focus:outline-none " type="add" name="add" placeholder="..." />
-                   <button type="submit" class="m-2 rounded bg-[#87b9a5] px-4 py-4 text-white">
-                   <IoPersonAddSharp />
-                  
-                </button>
-                </h3>
-              </div>
-            </div>
-          </div>
         </div>
-      </section>
-      
-    </div>
-  )
+    )
 }
 export default Agenda
