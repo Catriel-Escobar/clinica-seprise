c1e90d30e9f36cdb297bef761574134e752d3755
MaEugeniaB <eugebava@gmail.com>
2024-10-16 21:39:55 -0300

    rama actualizada Agenda e HC

--- breaking-code-medical-center/src/app/hc/page.js
+++ breaking-code-medical-center/src/app/hc/page.js
@@ -1,7 +1,9 @@
 "use client";
 
 import ConsultaModal from "@/Components/ConsultaModal";
-import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/solid";
+import { FaChevronUp } from "react-icons/fa";
+import { FaChevronDown } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 import axios from "axios";
 import { format } from "date-fns";
 import { es } from "date-fns/locale";
@@ -111,159 +113,178 @@ const HistoriaClinica = () => {
   };
 
   return (
-    <div className="container mx-auto p-6 flex flex-col md:flex-row  bg-white min-h-screen">
-      {/* Columna izquierda - Datos del paciente */}
-      <div className="md:w-1/3 md:pr-6 border-r border-gray-200">
-        <h2 className="text-2xl font-semibold mb-4">Historia Clínica</h2>
-        <div className="mb-6">
-          <label className="block text-gray-700 mb-2">DNI del paciente:</label>
+    <>
+      <div className="bg-cyan-100 text-center text-4xl font-bold h-[100px] py-7">
+        <h2 className="">Historia Clínica</h2>
+      </div>
+      <div className="flex justify-center mb-6 bg-cyan-100">
+        <div>
+          <label className="pr-8 font-medium">DNI del paciente:</label>
           <input
-            type="text"
+            type="search"
             value={dni}
             onChange={(e) => setDni(e.target.value)}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+            className="w-80 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
             placeholder="Introduce el DNI"
           />
           <button
+            type="submit"
             onClick={buscarPaciente}
-            className="mt-3 w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition-colors"
+            className="m-2 rounded bg-[#a7e6cc] px-4 py-4 text-white"
             disabled={loading}
           >
-            {loading ? "Buscando..." : "Buscar"}
+            {loading ? "" : ""}
+            <FaSearch />
           </button>
+          {mensaje && <p className="text-red-500 px-60 mt-2">{mensaje}</p>}
         </div>
-
-        {mensaje && <p className="text-red-500 mt-2">{mensaje}</p>}
-
-        {paciente && (
-          <div className="mt-6">
-            <h3 className="text-xl font-medium mb-4">Datos del Paciente</h3>
-            <p className="text-gray-800">
-              <strong>Nombre:</strong> {paciente.nombre}
-            </p>
-            <p className="text-gray-800">
-              <strong>Apellido:</strong> {paciente.apellido}
-            </p>
-            <p className="text-gray-800">
-              <strong>DNI:</strong> {paciente.dni}
-            </p>
-            <p className="text-gray-800">
-              <strong>Fecha de Nac:</strong>{" "}
-              {formatDate(paciente.fechaNacimiento)}
-            </p>
-            <p className="text-gray-800">
-              <strong>Obra Social:</strong> {paciente.obraSocial ? "Sí" : "No"}
-            </p>
-            <p className="text-gray-800">
-              <strong>Sexo:</strong>{" "}
-              {paciente.sexo === "M" ? "Masculino" : "Femenino"}
-            </p>
-            <p className="text-gray-800">
-              <strong>Tipo de Sangre:</strong> {paciente.tipoSangre}
-            </p>
-          </div>
-        )}
+        
       </div>
 
-      {/* Columna derecha - Consultas históricas */}
-      <div className="md:w-2/3 md:pl-6 mt-6 md:mt-0">
-        <div className="flex justify-between items-center mb-4">
-          <h3 className="text-xl font-medium">Consultas Históricas</h3>
-          {historiaClinica && (
-            <button
-              onClick={agregarConsulta}
-              className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition-colors"
-            >
-              Agregar Consulta
-            </button>
+      
+
+
+      <div className="contenedor flex ">
+        <div className="mx-auto flex-col md:flex-row bg-white min-h-screen">
+          {/* Columna izquierda - Datos del paciente */}
+
+          {paciente && (
+            <div className="mt-6 pt-5">
+              <h3 className="text-xl font-bold mb-4">Datos del Paciente</h3>
+              <p className="text-gray-800">
+                <strong>Nombre:</strong> {paciente.nombre}
+              </p>
+              <p className="text-gray-800">
+                <strong>Apellido:</strong> {paciente.apellido}
+              </p>
+              <p className="text-gray-800">
+                <strong>DNI:</strong> {paciente.dni}
+              </p>
+              <p className="text-gray-800">
+                <strong>Fecha de Nac:</strong>{" "}
+                {formatDate(paciente.fechaNacimiento)}
+              </p>
+              <p className="text-gray-800">
+                <strong>Obra Social:</strong> {paciente.obraSocial ? "Sí" : "No"}
+              </p>
+              <p className="text-gray-800">
+                <strong>Sexo:</strong>{" "}
+                {paciente.sexo === "M" ? "Masculino" : "Femenino"}
+              </p>
+              <p className="text-gray-800">
+                <strong>Tipo de Sangre:</strong> {paciente.tipoSangre}
+              </p>
+            </div>
+
           )}
         </div>
 
-        {historiaClinica ? (
-          historiaClinica.length > 0 ? (
-            <div className="overflow-x-auto">
-              <table className="min-w-full bg-white border">
-                <thead>
-                  <tr>
-                    <th className="px-4 py-2 border-b text-left text-gray-700">
-                      Fecha
-                    </th>
-                    <th className="px-4 py-2 border-b text-left text-gray-700">
-                      Médico
-                    </th>
-                    <th className="px-4 py-2 border-b text-left text-gray-700">
-                      Motivo
-                    </th>
-                    <th className="px-4 py-2 border-b text-left text-gray-700">
-                      Detalle
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {historiaClinica.map((registro) => (
-                    <tr
-                      key={registro.registro_id}
-                      className="hover:bg-gray-100"
-                    >
-                      <td className="px-4 py-2 border-b text-gray-800">
-                        {formatDate(registro.fecha)}
-                      </td>
-                      <td className="px-4 py-2 border-b text-gray-800">
-                        {registro.medico}
-                      </td>
-                      <td className="px-4 py-2 border-b text-gray-800">
-                        {registro.motivo}
-                      </td>
-                      <td className="px-4 py-2 border-b text-gray-800">
-                        <div className="flex items-center">
-                          <span
-                            onClick={() => toggleExpand(registro.registro_id)}
-                            className="cursor-pointer text-blue-600 hover:underline flex-grow"
-                          >
-                            {expandedConsultas.has(registro.registro_id)
-                              ? registro.detalle
-                              : truncateText(registro.detalle, 100)}
-                          </span>
-                          <button
-                            onClick={() => toggleExpand(registro.registro_id)}
-                            className="ml-2 focus:outline-none"
-                          >
-                            {expandedConsultas.has(registro.registro_id) ? (
-                              <ChevronUpIcon className="h-5 w-5 text-gray-500" />
-                            ) : (
-                              <ChevronDownIcon className="h-5 w-5 text-gray-500" />
-                            )}
-                          </button>
-                        </div>
-                      </td>
+
+        {/* Columna derecha - Consultas históricas */}
+        <div className="md:w-2/3 md:pl-6 mt-6 md:mt-0">
+          <div className="flex justify-between items-center px-5 mb-4">
+            <h3 className="text-xl font-bold">Consultas Históricas</h3>
+            {historiaClinica && (
+              <button
+                onClick={agregarConsulta}
+                className="bg-[#87b9a5] text-white px-4 py-2 rounded-md hover:bg-[#4b665b] transition-colors"
+              >
+                Agregar Consulta
+              </button>
+            )}
+          </div>
+
+          {historiaClinica ? (
+            historiaClinica.length > 0 ? (
+              <div className="overflow-x-auto pr-5">
+                <table className="min-w-full bg-white border">
+                  <thead>
+                    <tr>
+                      <th className="px-4 py-2 border-b text-left text-gray-700">
+                        Fecha
+                      </th>
+                      <th className="px-4 py-2 border-b text-left text-gray-700">
+                        Médico
+                      </th>
+                      <th className="px-4 py-2 border-b text-left text-gray-700">
+                        Motivo
+                      </th>
+                      <th className="px-4 py-2 border-b text-left text-gray-700">
+                        Detalle
+                      </th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+                  </thead>
+                  <tbody>
+                    {historiaClinica.map((registro) => (
+                      <tr
+                        key={registro.registro_id}
+                        className="hover:bg-gray-100"
+                      >
+                        <td className="px-4 py-2 border-b text-gray-800">
+                          {formatDate(registro.fecha)}
+                        </td>
+                        <td className="px-4 py-2 border-b text-gray-800">
+                          {registro.medico}
+                        </td>
+                        <td className="px-4 py-2 border-b text-gray-800">
+                          {registro.motivo}
+                        </td>
+                        <td className="px-4 py-2 border-b text-gray-800">
+                          <div className="flex items-center">
+                            <span
+                              onClick={() => toggleExpand(registro.registro_id)}
+                              className="cursor-pointer text-blue-600 hover:underline flex-grow"
+                            >
+                              {expandedConsultas.has(registro.registro_id)
+                                ? registro.detalle
+                                : truncateText(registro.detalle, 100)}
+                            </span>
+                            <button
+                              onClick={() => toggleExpand(registro.registro_id)}
+                              className="ml-2 focus:outline-none"
+                            >
+                              {expandedConsultas.has(registro.registro_id) ? (
+                                <FaChevronUp className="h-5 w-5 text-gray-500" />
+                              ) : (
+                                <FaChevronDown className="h-5 w-5 text-gray-500" />
+                              )}
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            ) : (
+              <p className="text-gray-600">
+                No hay consultas registradas para este paciente.
+              </p>
+            )
           ) : (
             <p className="text-gray-600">
-              No hay consultas registradas para este paciente.
+              Introduce el DNI para buscar al paciente y ver su historia clínica.
             </p>
-          )
-        ) : (
-          <p className="text-gray-600">
-            Introduce el DNI para buscar al paciente y ver su historia clínica.
-          </p>
+          )}
+          
+        </div>
+        {/* Modal para Agregar una consulta */}
+        {modalOpen && (
+          <ConsultaModal
+            isOpen={modalOpen}
+            onRequestClose={() => setModalOpen(false)}
+            onSuccess={refrescarHistoriaClinica}
+            historiaClinicaId={historiaClinica.id}
+            pacienteId={paciente.paciente_id}
+          />
         )}
+
+
       </div>
-      {/* Modal para Agregar una consulta */}
-      {modalOpen && (
-        <ConsultaModal
-          isOpen={modalOpen}
-          onRequestClose={() => setModalOpen(false)}
-          onSuccess={refrescarHistoriaClinica}
-          historiaClinicaId={historiaClinica.id}
-          pacienteId={paciente.paciente_id}
-        />
-      )}
-    </div>
+
+    </>
   );
 };
+
 
 export default HistoriaClinica;
