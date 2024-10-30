a5ec6ca00d2014a621b0d7b4f01abb8abb722e02
MaEugeniaB <eugebava@gmail.com>
2024-10-30 11:17:24 -0300

    logos y footer actualizados

--- breaking-code-medical-center/src/Components/UIComponents/Headers.js
+++ breaking-code-medical-center/src/Components/UIComponents/Headers.js
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import logo from "@/assets/logo.png";
+import logoSP from "@/assets/logoSP.png";
 import NavLinks from "./NavLinks"; 
 import UserArea from "./UserArea"; 
 import { useAuth } from "@/contexts/AuthContext"; 
@@ -12,7 +12,7 @@ const Headers = () => {
   return (
     <div className="flex justify-around items-center h-[90px] bg-[#87b9a5] text-white font-semibold">
       <Link href="/" aria-label="Ir a la página de inicio" className="h-[70px] w-[100px]">
-        <Image src={logo} alt="Logo" className="w-full h-auto" width="90" height="70" priority />
+        <Image src={logoSP} alt="Logo" className="w-full h-auto" width="90" height="70" priority />
       </Link>
       <NavLinks user={user} />
       <UserArea user={user} logout={logout} />
